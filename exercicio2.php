304afa81a4d4f9ab312d6366bb7f3615015fb7cc
andreerfr <andreerci@gmail.com>
2025-08-22 12:30:20 -0300

    2

---
+++ exercicio2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 01 - Par ou Impar</title>
+</head>
+<body>
+<form method="POST" action="">
+        <label for="numero">Digite um número:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar_par_impar">Calcular</button>
+</form>
+
+
+
+</body>
+</html>
