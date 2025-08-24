f563ee4bb40a7e497930ceab2cc998a88e83af60
Leandro Felipe Moreira <leandroofelipee@gmail.com>
2025-08-24 17:56:13 -0300

    4 final?

--- exercicio 4.php
+++ exercicio 4.php
@@ -6,6 +6,26 @@
     <title>Exercicio 4</title>
 </head>
 <body>
-    
+    <form method="POST" action="">
+        <label for="numero">Digite um número:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="fatorial">fatorialzado</button>
+</form>
+
+<?php
+  if ($_SERVER['REQUEST_METHOD']=== 'POST') {
+        if(isset($_POST['fatorial'])) {
+            $numero = $_POST['numero'];
+            for ($i = $numero; $i <= $numero; $i--) {
+                $resultado = $i *  $resultado;
+                $resultadofinal = $resultadofinal + $resultado;
+            }
+ echo "$resultadofinal";
+                }
+
+ }
+
+
+?>
 </body>
 </html>
